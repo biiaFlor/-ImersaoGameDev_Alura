bf4955247ba6d8f2a694581b6286178d36cae2f5
unknown <biiaflor_ifsp@hotmail.com>
2020-06-23 19:31:14 -0300

    Versao Flappy Bird

--- sketch.js
+++ sketch.js
@@ -1,27 +1,95 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let somDoJogo;
 
+let cenario;
+let personagem;
+let inimigo;
+
+let contador = 0;
+
+const matrizInimigo = [
+  [0, 0],
+  [105, 0],
+  [210, 0],
+  [315, 0],
+  [0, 104],
+  [105, 104],
+  [210, 104],
+  [315, 104],
+  [0, 208],
+  [105, 208],
+  [210, 208],
+  [315, 208],
+  [0, 312],
+  [105, 312],
+  [210, 312],
+  [315, 312],
+  [0, 409],
+  [105, 409],
+  [210, 409],
+  [315, 409],
+  [0, 503],
+  [105, 503],
+  [210, 503],
+  [315, 503],
+  [0, 609],
+  [105, 609],
+  [210, 609],
+  [315, 609],
+]
+
+const matrizPersonagem = [
+      [0,0],
+      [220,0],
+      [440,0],
+      [660,0],
+      [0,270],
+      [220,270],
+      [440,270],
+      [660,270],
+      [0,540],
+      [220,540],
+      [440,540],
+      [660,540],
+      [0,810],
+      [220,810],
+      [440,810],
+      [660,810],
+    ]
+
 function preload(){
-  imagemPersonagem = loadImage( 'imagens/personagem/correndo.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
   imagemCenario = loadImage( 'imagens/cenario/floresta.png');
+  imagemInimigo = loadImage( 'imagens/inimigos/gotinha.png');
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
 }
 
-function setup() { //executada uma vez antes do jogo começar
+function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Persongem(imagemPersonagem);
+  personagem = new Persongem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width-52, 52, 52, 104, 104);
+  
   frameRate(40);
-  somDoJogo.loop();
+  //somDoJogo.loop();
 }
 
-function draw() { //Renderizada constantemente no navegador
-  
-  //background(220); -> Define uma cor solida para o fundo
-  //circle(mouseX,mouseY,250); -> cria um circulo que acompanha a posição do mouse
+function keyPressed(){
+  contador++;
+  if(key === 'ArrowUp'){
+     personagem.pula();
+  }
+}
+
+function draw() { 
   cenario.exibe();
   cenario.move();
   
   personagem.exibe();
+  personagem.aplicaGravidade();
+  
+  inimigo.exibe();
+  inimigo.move();
 }
